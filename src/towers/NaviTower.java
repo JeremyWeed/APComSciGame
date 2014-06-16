be8464c0920a789829f8497639633afe04ab2282
JHGZhou <cjhuahoo@yahoo.com>
2014-06-15 22:09:35 -0400

    NERFBAT

--- src/towers/NaviTower.java
+++ src/towers/NaviTower.java
@@ -11,7 +11,7 @@ public class NaviTower extends Tower
     }
     public NaviTower(Map map,int x, int y)
     {
-        super(map,"resources/Navi.png", "resources/Navi Splash.png", "resources/Navi Flash.png", 350,14,1,true,70);
+        super(map,"resources/Navi.png", "resources/Navi Splash.png", "resources/Navi Flash.png", 175,14,1,true,70);
         add(x,y);
     }
 
